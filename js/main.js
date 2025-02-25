1e910c95447a980dca15e91ba47160740430afdc
Adam2252 <danreal04@mail.ru>
2025-02-25 13:05:54 +0300

    fix: height navbar scrol

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navbar = document.querySelector(".navbar")
+const logo = document.querySelector(".logo-svg use")
 window.addEventListener('scroll', () => {
     if(this.scrollY > 1){
         navbar.classList.add('navbar-light')
+        logo.href.baseVal = "img/icons.svg#logo-li2ght";
     } else {
         navbar.classList.remove('navbar-light')
+        logo.href.baseVal = "img/icons.svg#logo-light";
     }
 })
