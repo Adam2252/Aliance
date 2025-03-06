0e618e0b289d8595a68cbcb1ac1381afc6d6cbc3
Adam2252 <danreal04@mail.ru>
2025-03-06 20:50:47 +0300

    Add: pp

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@ const navbar = document.querySelector(".navbar")
 const logo = document.querySelector(".logo-svg use")
 const mMenuToggle = document.querySelector(".mobile-menu-toggle")
 const menu = document.querySelector(".mobile-menu")
+
+const openMenu = (event) => {
+    menu.classList.add("is-open");
+    document.body.style.overflow="hidden";
+    navbar.classList.add('navbar-light')
+    logo.href.baseVal = "img/icons.svg#logo-li2ght";
+};
+
+const closeMenu = (event) => {
+    menu.classList.remove("is-open");
+    document.body.style.overflow="";
+    navbar.classList.remove('navbar-light')
+    logo.href.baseVal = "img/icons.svg#logo-light";
+};
+
 window.addEventListener('scroll', () => {
-    if(this.scrollY > 1){
+    if(this.scrollY > 10){
         navbar.classList.add('navbar-light')
         logo.href.baseVal = "img/icons.svg#logo-li2ght";
     } else {
@@ -14,5 +29,10 @@ window.addEventListener('scroll', () => {
 
 mMenuToggle.addEventListener("click", (event) => {
     event.preventDefault();
-    menu.classList.toggle("is-open");
+    if (menu.classList.contains("is open")){
+        closeMenu();
+    } else{
+        openMenu();
+    }
 })
+
