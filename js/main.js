dcbc173456a44b8a533f5582310fa7e407e7a103
Adam2252 <danreal04@mail.ru>
2025-04-08 15:16:44 +0300

    pp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const navbar = document.querySelector(".navbar")
-const logo = document.querySelector(".logo-svg use")
+const logoLight = document.querySelector(".logo-logo-light")
+const logo = document.querySelector(".logo")
 const mMenuToggle = document.querySelector(".mobile-menu-toggle")
 const menu = document.querySelector(".mobile-menu")
 
@@ -7,23 +8,25 @@ const openMenu = (event) => {
     menu.classList.add("is-open");
     document.body.style.overflow="hidden";
     navbar.classList.add('navbar-light')
-    logo.href.baseVal = "img/icons.svg#logo-li2ght";
+    logo.href.baseVal = "img/icons.svg#logo-li2ght"
 };
 
 const closeMenu = (event) => {
     menu.classList.remove("is-open");
     document.body.style.overflow="";
     navbar.classList.remove('navbar-light')
-    logo.href.baseVal = "img/icons.svg#logo-light";
+    logo.href.baseVal = "img/icons.svg#logo-light"
 };
 
 window.addEventListener('scroll', () => {
     if(this.scrollY > 10){
         navbar.classList.add('navbar-light')
-        logo.href.baseVal = "img/icons.svg#logo-li2ght";
+        logo.style.display = "block";
+        logoLight.style.display = "none";
     } else {
-        navbar.classList.remove('navbar-light')
-        logo.href.baseVal = "img/icons.svg#logo-light";
+        navbar.classList.remove('navbar-light');
+        logo.style.display = "none";
+        logoLight.style.display = "block";
     }
 });
 
@@ -36,13 +39,36 @@ mMenuToggle.addEventListener("click", (event) => {
     }
 })
 
+const swiperSteps = new Swiper('.steps-slider', {
+  speed: 400,
+  slidesPerView: 1,
+  autoHeight: true,
+  navigation: {
+      nextEl: '.steps-button-next',
+      prevEl: '.steps-button-prev',
+    }, 
+    breakpoints: {
+      // when window width is >= 640px
+      576: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      1024: {
+        slidesPerView: 4,
+      },
+    }
+});   
+
 const swiper = new Swiper('.swiper', {
     speed: 400,
-    autoHeight: true,
     slidesPerView: 1,
     navigation: {
-        nextEl: '.slider-buttons-next, .swiper-button-next',
-        prevEl: '.slider-buttons-prev, .swiper-button-prev',
+        nextEl: '.slider-buttons-next',
+        prevEl: '.slider-buttons-prev',
       }, 
       breakpoints: {
         // when window width is >= 320px
