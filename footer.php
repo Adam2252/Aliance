e5660192892ccd0aac41418119ee4fee16256676
Adam2252 <danreal04@mail.ru>
2025-06-10 17:21:38 +0300

    Gist

---
+++ footer.php
@@ -0,0 +1,166 @@
+<section class="cta">
+        <div class="bg-grey">
+            <img src="img/Frame 48.png" alt="CTA" class="cta-image">
+            <div class="cta-form-wrapper container">
+                <form action="#" class="cta-form">
+                    <h2 class="cta-form-title section-title">Хотите сотрудничать?</h2>
+                    <p class="cta-form-text">
+                        Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы и поможем даже в самых сложных случаях
+                    </p>
+                    <div class="input-group-wrapper">
+                        <div class="input-group">
+                            <input id="user-name" type="text" class="input" placeholder=" "/>
+                            <label  class="input-group-label" for="user-name">Имя</label>
+                        </div>
+                        <div class="input-group">
+                            <input id="user-phone" type="tel" class="input phone-mask" placeholder=" "/>
+                            <label class="input-group-label" for="user-phone">Номер телефона</label>
+                        </div>
+                    </div>
+                    <div class="cta-form-footer">
+                        <button type="submit" class="button cta-form-button">Отправить заявку</button>
+                        <div class="notify">
+                            <svg  width="14" height="14" class="notify-icon">
+                                <use href="img/icons.svg#shield"></use>
+                            </svg>
+                            <p class="notify-text">Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную конфиденциальность информации!</p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-top">
+                <svg class="logo-logo footer-logo" width="110" height="44">
+                    <use href="img/icons.svg#logo"></use>
+                </svg>
+                <a href="tel:+74996861014" class="footer-phone">+7 (499) 686-10-14</a>
+                <div class="footer-info">
+                    <svg class="phone-icon" width="24" height="24">
+                        <use href="img/icons.svg#phone"></use>
+                    </svg>
+                    <address class="footer-info-adress">г. Москва, Холодильный пер. 4к1с8</address>
+                </div>
+                <div class="footer-info">
+                    <svg class="phone-icon" width="24" height="24">
+                        <use href="img/icons.svg#phone"></use>
+                    </svg>
+                    <a href="mailto:a.dragunov@tdaliance.ru" class="footer-info-email">a.dragunov@tdaliance.ru</a>
+                    <a href="#" class="footer-social-link">
+                        <svg class="footer-social-icon" width="24" height="24">
+                            <use href="img/icons.svg#VK"></use>
+                        </svg>
+                    </a>
+                    <a href="#" class="footer-social-link">
+                        <svg class="footer-social-icon" width="24" height="24">
+                            <use href="img/icons.svg#insta"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr class="footer-seporator">
+        <div class="container">
+            <div class="footer-bottom">
+                <div class="footer-menu-wrapper">
+                    <h2 class="footer-menu-title">Контрактное производство</h2>
+                    <ul class="footer-menu-list footer-menu-columns-2">
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Автомобильная химия</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Бытовая химия</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Дезинфицирующие средства</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Пищевые аэрозоли</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Косметическая продукция</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Краски аэрозольные</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-menu-wrapper">
+                    <h2 class="footer-menu-title">Собственные марки</h2>
+                    <ul class="footer-menu-list">
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Автохимия AG-Tech</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link">Автохимия AP</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-menu-wrapper">
+                    <ul class="footer-menu-list">
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link footer-menu-link-bold">О компании</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link footer-menu-link-bold">Новости</a>
+                        </li>
+                        <li class="footer-menu-item">
+                            <a href="#" class="footer-menu-link footer-menu-link-bold">Контакты</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr  class="footer-seporator">
+        <div class="container">
+            <div class="footer-wrapper">
+                <div class="footer-legal">
+                <p class="copiright">&copy; 2022 «Aliance Production». Все права защищены.</p>
+                <a href="#" class="footer-policy">Политики конфиденциальности</a>
+            </div>
+            <div class="footer-author">
+                <span class="made-in">
+                    Сделанно в
+                </span>
+                <svg class="made-in-icon"width="52" height="11">
+                    <use href="img/icons.svg#ruso"></use>
+                </svg>
+            </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </footer>
+    <div class="modal">
+        <div class="modal-dilog">
+            <h2 class="modal-title">Есть вопросы?</h2>
+            <a href="#" class="modal-close">
+                <svg class="close-icon"width="24" height="24">
+                    <use href="img/icons.svg#close"></use>
+                </svg>
+            </a>
+            <p class="modal-text">Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит на все интересующие вопросы и поможем даже в самых сложных случаях!</p>
+            <form action="#" class="modal-form">
+                <div class="input-group-wrapper input-group-wertical">
+                        <div class="input-group modal-input-group">
+                            <input id="modal-user-name" type="text" class="input modal-input" placeholder=" "/>
+                            <label  class="input-group-label modal-input-label" for="modal-user-name">Имя</label>
+                        </div>
+                        <div class="input-group modal-input-group">
+                            <input id="modal-user-phone" type="tel" class="input phone-mask modal-input" placeholder=" "/>
+                            <label class="input-group-label modal-input-label" for="modal-user-phone">Номер телефона</label>
+                        </div>
+                    </div>
+                    <div class="cta-form-footer modal-flex">
+                        <button type="submit" class="button modal-form-button">Отправить заявку</button>
+                        <div class="notify">
+                            <svg  width="14" height="14" class="notify-icon">
+                                <use href="img/icons.svg#shield"></use>
+                            </svg>
+                            <p class="notify-text">Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную конфиденциальность информации!</p>
+                        </div>
+                    </div>
+            </form>
+        </div>
+    </div>
